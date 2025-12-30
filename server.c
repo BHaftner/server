bcd94c18e312cc4be68dd88da9a9b20e870af526
BHaftner <brockhaftner@gmail.com>
2025-12-30 03:08:10 -0800

    Diabolical amount of tweaking and fixing and changing. Split methods up, made parser more robust, swapped to using a pool, and many other things. I should have done smaller git updates however just kept chugging away. 3am, time for sleep surely...

--- server.c
+++ server.c
@@ -10,14 +10,78 @@
 #include <sys/stat.h>
 #include <sys/epoll.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 
 #define PORT "3490"
-#define MAX_EVENTS 10
-#define BACKLOG 10
-#define BUFFER_SIZE 65536
+#define MAX_EVENTS 1024
+#define BACKLOG 1024
+#define REQUEST_BUFFER_SIZE 8192
+#define HEADER_BUFFER_SIZE 1024
+#define CONNECTION_POOL_SIZE 10000
+
+typedef enum {
+	STATE_READ_REQUEST,
+	STATE_PARSE_REQUEST,
+	STATE_SEND_HEADER,
+	STATE_SEND_FILE,
+	STATE_DONE
+} conn_state_t;
+
+typedef struct {
+	int fd;
+	conn_state_t state;
+	int in_use;
+
+	char request_buffer[REQUEST_BUFFER_SIZE];
+	size_t request_len;
+
+	char method[16];
+	char uri[256];
+	char filepath[512];
+
+	char response_header[HEADER_BUFFER_SIZE];
+	size_t header_len;
+	size_t header_sent;
+
+	int file_fd;
+	off_t file_size;
+	off_t file_offset;
+} connection_t;
+
+connection_t connection_pool[CONNECTION_POOL_SIZE];
+connection_t *free_list[CONNECTION_POOL_SIZE];
+int free_list_count = CONNECTION_POOL_SIZE;
+
+void init_connection_pool(void) {
+	
+	for (int i = 0; i < CONNECTION_POOL_SIZE; i++) {
+		memset(&connection_pool[i], 0, sizeof(connection_t));
+		connection_pool[i].in_use = 0;
+		connection_pool[i].file_fd = -1;
+		free_list[i] = &connection_pool[i];
+	}
+}
+
+connection_t *pool_alloc_connection(void) {
+	
+	if (free_list_count <= 0) return NULL;
+	connection_t *conn = free_list[--free_list_count];
+	memset(conn, 0, sizeof(connection_t));
+	conn->in_use = 1;
+	conn->file_fd = -1;
+	return conn;
+}	
+
+void pool_free_connection(connection_t *conn) {
+    
+	conn->in_use = 0;
+    if (free_list_count < CONNECTION_POOL_SIZE) {
+        free_list[free_list_count++] = conn;
+    }
+}
 
 // makes a non-blocking socket
 int set_nonblocking(int sockfd) {
@@ -31,137 +95,302 @@ int set_nonblocking(int sockfd) {
 // get content-type
 const char *get_mime_type(const char *filename) {
 
-	const char *dot = strrchr(filename, '.');
-	if (!dot) return "text/plain";
-	if (strcmp(dot, ".html") == 0) return "text/html";
-	if (strcmp(dot, ".css") == 0) return "text/css";
-	if (strcmp(dot, ".js") == 0) return "application/javascript";
-	if (strcmp(dot, ".jpg") == 0) return "image/jpeg";
-	if (strcmp(dot, ".png") == 0) return "image/png";
-	if (strcmp(dot, ".gif") == 0) return "image/gif";
-	return "text/plain";
+    const char *dot = strrchr(filename, '.');
+    if (!dot) return "text/plain";
+    
+    switch(dot[1]) {
+        case 'h':
+            if (strcmp(dot, ".html") == 0) return "text/html";
+            break;
+        case 'c':
+            if (strcmp(dot, ".css") == 0) return "text/css";
+            break;
+        case 'j':
+            if (strcmp(dot, ".js") == 0) return "application/javascript";
+            if (strcmp(dot, ".jpg") == 0 || strcmp(dot, ".jpeg") == 0) 
+                return "image/jpeg";
+            break;
+        case 'p':
+            if (strcmp(dot, ".png") == 0) return "image/png";
+            break;
+        case 'g':
+            if (strcmp(dot, ".gif") == 0) return "image/gif";
+            break;
+    }
+    return "text/plain";
 }
 
 // 404 response
-void send_404(int client_fd) {
+void send_404_and_close(connection_t *conn, int epoll_fd) {
 
-	const char *response = "HTTP/1.1 404 NOT FOUND\r\n"
-						   "Content-Type: text/plain\r\n"
-						   "Content-Length: 13\r\n"
-						   "\r\n"
-						   "404 Not Found";
+	const char *response = 
+		"HTTP/1.1 404 NOT FOUND\r\n"
+	    "Content-Type: text/plain\r\n"
+	    "Content-Length: 13\r\n"
+		"Connection: close\r\n"
+	    "\r\n"
+	    "404 Not Found";
 
-	send(client_fd, response, strlen(response), 0);
+	send(conn->fd, response, strlen(response), 0);
+	epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->fd, NULL);
+	close(conn->fd);
+	if (conn->file_fd >= 0) close(conn->file_fd);
+	pool_free_connection(conn);
 }
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+connection_t *create_connection(int client_fd) {
+	
+	connection_t *conn = pool_alloc_connection();
+	if (!conn) return NULL;
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	conn->fd = client_fd;
+	conn->state = STATE_READ_REQUEST;
+
+	return conn;
 }
 
-void handle_client(int client_fd, int epoll_fd) {
+void destroy_connection(connection_t *conn, int epoll_fd) {
+	
+	epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->fd, NULL);
+	close(conn->fd);
+	if (conn->file_fd >= 0) close(conn->file_fd);
+	pool_free_connection(conn);
+}
 
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+int parse_and_prepare_response(connection_t *conn) {
+	
+	if (sscanf(conn->request_buffer, "%15s %255s", conn->method, conn->uri) != 2) return -1;
+    printf("Request: %s %s\n", conn->method, conn->uri);
 
-	if (bytes_received < 0) {
-		if (errno != EAGAIN && errno != EWOULDBLOCK) {
-			perror("recv");
-			epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-			close(client_fd);
-		}
-		return;
-	}
+    if (strcmp(conn->method, "GET") != 0) return -1;
 
-	if (bytes_received == 0) {
-		epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-		close(client_fd);
-		return;
-	}
+    if (strstr(conn->uri, "..") != NULL) {
+        return -1;
+    }
 
-	buffer[bytes_received] = '\0';
+    if (strcmp(conn->uri, "/") == 0) {
+        snprintf(conn->filepath, sizeof(conn->filepath), "www/index.html");
+    } else {
+        if (strlen(conn->uri) > (sizeof(conn->filepath) - 4)) return -1;
+        snprintf(conn->filepath, sizeof(conn->filepath), "www%s", conn->uri);
+    }
 
-	char method[16], uri[256], protocol[16];
-
-	sscanf(buffer, "%15s %255s %15s", method, uri, protocol);
-	printf("Request: %s %s\n", method, uri);
-
-	// security check for directory traversal
-	if (strstr(uri, "..")) {
-		send_404(client_fd);
-		epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-		close(client_fd);
-		return;
-	}
-
-	char filepath[512];
-	if (strcmp(uri, "/") == 0) {
-		sprintf(filepath, "www/index.html");
-	} else {
-		sprintf(filepath, "www%s", uri);
-	}
-
-	int file_fd = open(filepath, O_RDONLY);
-	if (file_fd < 0) {
-		send_404(client_fd);
-		epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-		close(client_fd);
-		return;
-	}
+	conn->file_fd = open(conn->filepath, O_RDONLY);
+	if (conn->file_fd < 0) return -1;
 
 	struct stat file_stat;
-	if (fstat(file_fd, &file_stat) < 0) {
-		perror("fstat");
-		close(file_fd);
-		epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-		close(client_fd);
+	if (fstat(conn->file_fd, &file_stat) < 0) {
+		close(conn->file_fd);
+		conn->file_fd = -1;
+		return -1;
+	}
+	conn->file_size = file_stat.st_size;
+
+	const char *mime_type = get_mime_type(conn->filepath);
+	conn->header_len = snprintf(conn->response_header, sizeof(conn->response_header),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: %s\r\n"
+        "Content-Length: %ld\r\n"
+        "Connection: keep-alive\r\n"
+        "Keep-Alive: timeout=5, max=100\r\n"
+        "\r\n",
+        mime_type, conn->file_size);
+
+	conn->header_sent = 0;
+	conn->file_offset = 0;
+
+	return 0;
+}
+
+int handle_read_request(connection_t *conn) {
+	
+	while (1) {
+
+		size_t remain = REQUEST_BUFFER_SIZE - conn->request_len - 1;
+		if (remain == 0) return -1;
+
+		ssize_t n = recv(conn->fd, conn->request_buffer + conn->request_len, remain, 0);
+
+		if (n < 0) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK) {
+				if (strstr(conn->request_buffer, "\r\n\r\n") != NULL) return 1;
+				return 0;
+			}
+			return -1;
+		}
+
+		if (n == 0) return -1;
+
+		conn->request_len += n;
+		conn->request_buffer[conn->request_len] = '\0';
+
+		if (strstr(conn->request_buffer, "\r\n\r\n") != NULL) return 1;
+	}
+}
+
+int handle_send_header(connection_t *conn) {
+	
+	while (conn->header_sent < conn->header_len) {
+
+		ssize_t n = send(conn->fd, conn->response_header + conn->header_sent, conn->header_len - conn->header_sent, 0);
+
+		if (n < 0) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK) return 0;
+			return -1;
+		}
+
+		conn->header_sent += n;
+	}
+	return 1;
+}
+
+int handle_send_file(connection_t *conn) {
+
+    while (conn->file_offset < conn->file_size) {
+
+        ssize_t n = sendfile(conn->fd,conn->file_fd, &conn->file_offset, conn->file_size - conn->file_offset);
+
+        if (n < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                return 0;
+            }
+            return -1;
+        }
+
+        if (n == 0) {
+            break;
+        }
+    }
+    return 1;
+}
+
+void reset_for_next_request(connection_t *conn) {
+
+	char *end_of_header = strstr(conn->request_buffer, "\r\n\r\n");
+	if (!end_of_header) {
+
+		conn->request_len = 0;
+		conn->state = STATE_READ_REQUEST;
 		return;
 	}
-	off_t file_size = file_stat.st_size;
 
-	const char *mime_type = get_mime_type(filepath);
-	char header[1024];
-	int header_len = snprintf(header, sizeof(header),
-			"HTTP/1.1 200 OK\r\n"
-			"Content-Type: %s\r\n"
-			"Content-Length: %ld\r\n"
-			"\r\n",
-			mime_type, file_size);
-	send(client_fd, header, header_len, 0);
-	
-	off_t offset = 0;
-	ssize_t sent_bytes = 0;
+	size_t header_size = (end_of_header - conn->request_buffer) + 4;
+	size_t leftover = conn->request_len - header_size;
 
-	while (offset < file_size) {
+	if (leftover > 0) {
 
-		ssize_t res = sendfile(client_fd, file_fd, &offset, file_size - offset);
+		memmove(conn->request_buffer, conn->request_buffer + header_size, leftover);
+		
+		conn->request_len = leftover;
+		conn->request_buffer[conn->request_len] = '\0';
 
-		if (res < 0) {
-			if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
-			perror("sendfile");
-			break;
+		if (strstr(conn->request_buffer, "\r\n\r\n") != NULL) {
+			conn->state = STATE_PARSE_REQUEST;
+		} else {
+			conn->state = STATE_READ_REQUEST;
 		}
-		sent_bytes += res;
+	} else {
+		
+		conn->request_len = 0;
+		conn->state = STATE_READ_REQUEST;
 	}
-	
-	close(file_fd);
-	epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-	close(client_fd);
+}
+
+void handle_connection(connection_t *conn, int epoll_fd, uint32_t events) {
+
+    struct epoll_event ev;
+    int result;
+    int process_next_request = 0;
+
+    do {
+        process_next_request = 0;
+
+        if (events & EPOLLIN) {
+            if (conn->state == STATE_READ_REQUEST) {
+                result = handle_read_request(conn);
+                if (result < 0) {
+                    destroy_connection(conn, epoll_fd);
+                    return;
+                }
+                if (result == 1) {
+                    conn->state = STATE_PARSE_REQUEST;
+                }
+            }
+        }
+
+        if (conn->state == STATE_PARSE_REQUEST) {
+            if (parse_and_prepare_response(conn) < 0) {
+                send_404_and_close(conn, epoll_fd);
+                return;
+            }
+
+            int flag = 1;
+            setsockopt(conn->fd, IPPROTO_TCP, TCP_CORK, &flag, sizeof(flag));
+
+            conn->state = STATE_SEND_HEADER;
+
+            events |= EPOLLOUT;
+
+            ev.events = EPOLLOUT | EPOLLET;
+            ev.data.ptr = conn;
+            epoll_ctl(epoll_fd, EPOLL_CTL_MOD, conn->fd, &ev);
+        }
+
+        if (events & EPOLLOUT) {
+            if (conn->state == STATE_SEND_HEADER) {
+                result = handle_send_header(conn);
+                if (result < 0) {
+                    destroy_connection(conn, epoll_fd);
+                    return;
+                }
+                if (result == 1) {
+                    conn->state = STATE_SEND_FILE;
+                }
+            }
+
+            if (conn->state == STATE_SEND_FILE) {
+                result = handle_send_file(conn);
+                if (result < 0) {
+                    destroy_connection(conn, epoll_fd);
+                    return;
+                }
+                if (result == 1) {
+                    int flag = 0;
+                    setsockopt(conn->fd, IPPROTO_TCP, TCP_CORK, &flag, sizeof(flag));
+
+                    close(conn->file_fd);
+                    conn->file_fd = -1;
+
+                    reset_for_next_request(conn);
+
+                    if (conn->state == STATE_PARSE_REQUEST) {
+                        process_next_request = 1;
+                        events = 0;
+                    } else {
+                        ev.events = EPOLLIN | EPOLLET;
+                        ev.data.ptr = conn;
+                        epoll_ctl(epoll_fd, EPOLL_CTL_MOD, conn->fd, &ev);
+                    }
+                }
+            }
+        }
+
+    } while (process_next_request);
+
+    if (events & (EPOLLERR | EPOLLHUP)) {
+        destroy_connection(conn, epoll_fd);
+    }
 }
 
 int main(void) {
 
-	int listen_fd, new_fd;
+	int listen_fd;
 	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr;
-	socklen_t sin_size;
 	int yes=1;
 	int rv;
+
+	init_connection_pool();
+	printf("Initialized connection pool with %d connections\n", CONNECTION_POOL_SIZE);
 
 	signal(SIGPIPE, SIG_IGN);
 
@@ -178,24 +407,26 @@ int main(void) {
 	for(p = servinfo; p != NULL; p = p->ai_next) {
 		if ((listen_fd = socket(p->ai_family, p->ai_socktype,
 				p->ai_protocol)) == -1) {
-			perror("server: socket");
+			perror("socket");
 			continue;
 		}
 
 		if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &yes,
 				sizeof(int)) == -1) {
 			perror("setsockopt");
-			exit(1);
+			close(listen_fd);
+			continue;
 		}
 
 		if (set_nonblocking(listen_fd) == -1) {
 			perror("set_nonblocking");
-			exit(1);
+			close(listen_fd);
+			continue;
 		}
 
 		if (bind(listen_fd, p->ai_addr, p->ai_addrlen) == -1) {
 			close(listen_fd);
-			perror("server: bind");
+			perror("bind");
 			continue;
 		}
 
@@ -206,69 +437,95 @@ int main(void) {
 
 	if (p == NULL)  {
 		fprintf(stderr, "server: failed to bind\n");
-		exit(1);
+		return 1;
 	}
 
 	if (listen(listen_fd, BACKLOG) == -1) {
 		perror("listen");
-		exit(1);
+		return 1;
 	}
 
 	int epoll_fd = epoll_create1(0);
 	if (epoll_fd == -1) {
 		perror("epoll_create1");
-		exit(1);
+		return 1;
 	}
 
-	struct epoll_event event;
-	struct epoll_event events[MAX_EVENTS];
+	struct epoll_event ev, events[MAX_EVENTS];
+	ev.events = EPOLLIN;
+	ev.data.fd = listen_fd;
 
-	event.data.fd = listen_fd;
-	event.events = EPOLLIN;
-
-	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &event) == -1) {
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev) == -1) {
 		perror("epoll_ctl: listen_fd");
-		exit(1);
+		return 1;
 	}
 
-	printf("server: waiting for connections (epoll mode)...\n");
+	printf("Server listening on port %s...\n", PORT);
 
 	while(1) {  // main accept() loop
 		
 		int n_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
 
 		if (n_events == -1) {
+			if (errno == EINTR) continue;
 			perror("epoll_wait");
-			exit(1);
+			break;
 		}
 
 		for (int i = 0; i < n_events; i++) {
-		
 			if (events[i].data.fd == listen_fd) {
-				sin_size = sizeof their_addr;
-				new_fd = accept(listen_fd, (struct sockaddr *)&their_addr, &sin_size);
-				if (new_fd == -1) {
-					perror("accept");
-					continue;
-				}
+                while (1) {
+                    struct sockaddr_storage client_addr;
+                    socklen_t addr_len = sizeof(client_addr);
 
-				if (set_nonblocking(new_fd) == -1) {
-					perror("set_nonblocking");
-					exit(1);
-				}
+                    int client_fd = accept(listen_fd,
+                                          (struct sockaddr *)&client_addr,
+                                          &addr_len);
 
-				event.data.fd = new_fd;
-				event.events = EPOLLIN;
-				if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_fd, &event) == -1) {
-                    perror("epoll_ctl: new_fd");
-                    exit(1);
+                    if (client_fd == -1) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            break;
+                        }
+                        perror("accept");
+                        break;
+                    }
+
+                    if (set_nonblocking(client_fd) == -1) {
+                        perror("set_nonblocking");
+                        close(client_fd);
+                        continue;
+                    }
+
+                    int flag = 1;
+                    setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY,
+                              &flag, sizeof(flag));
+
+                    connection_t *conn = create_connection(client_fd);
+                    if (!conn) {
+                        fprintf(stderr, "Connection pool exhausted!\n");
+                        close(client_fd);
+                        continue;
+                    }
+
+                    ev.events = EPOLLIN | EPOLLET;
+                    ev.data.ptr = conn;
+
+                    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) == -1) {
+                        perror("epoll_ctl: client_fd");
+                        pool_free_connection(conn);
+                        close(client_fd);
+                        continue;
+                    }
                 }
-			} else {
-				handle_client(events[i].data.fd, epoll_fd);
-			}
-		} 
-	}
+            }
+            else {
+                connection_t *conn = (connection_t *)events[i].data.ptr;
+                handle_connection(conn, epoll_fd, events[i].events);
+            }
+        }
+    }
 
-	close(listen_fd);
-	return 0;
+    close(listen_fd);
+    close(epoll_fd);
+    return 0;
 }
